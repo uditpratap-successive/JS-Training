0ef14192a10f7476f1c60d78b087593aaf2c6a1b
Udit Pratap <udit.pratap@uditpratapr-425.successivetech.com>
2024-03-05 14:23:18 +0530

    Day2 Assignment updated

--- day.2/All question using fat arrow function.js
+++ day.2/All question using fat arrow function.js
@@ -28,7 +28,7 @@ console.log(square(4));
 
 //Question 5:- //Program to return the sum of the two number.
 
-function sum(a, b) {
+const  sum=(a, b)=> {
     return a + b;
 
 }
@@ -38,7 +38,7 @@ console.log(sum(4, 5))
 
 //Question 6:- Program to check the number is even or odd and retrun true or false on that basis.
 
-function checker(num) {
+const checker=(num) =>{
     if (num % 2 == 0) {
         return true;
     }
