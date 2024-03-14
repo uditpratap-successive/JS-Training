889e840fe21a1966b6e0dc39ab49f800454f9781
Udit Pratap <udit.pratap@uditpratapr-425.successivetech.com>
2024-03-14 11:21:20 +0530

    updated assignment

--- day.2/ques2.js
+++ day.2/ques2.js
@@ -1,13 +1,7 @@
 //Program to take the input from the user's name ,age, phone_number and print it output.
 
-<<<<<<< HEAD
 let user_name="Udit Pratap Singh"
 let age=23;
 let phone_number=999999999;
-=======
-let user_name="Udit Pratap Singh
-let age=22;
-let phone_number=839393833939933;
->>>>>>> df3abd7ec50fb90d482ba2efeeaf3f9ac57f5204
 
 console.log(`The name of user is ${user_name} and the age of user is ${age} and phone number is ${phone_number}`);  
