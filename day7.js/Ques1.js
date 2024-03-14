075912a4a9a87de11de10103363b1edfbc29f29f
Udit Pratap <udit.pratap@uditpratapr-425.successivetech.com>
2024-03-14 11:49:42 +0530

    Updated Day 7 assignment

--- day7.js/Ques1.js
+++ day7.js/Ques1.js
@@ -1,23 +1,27 @@
 //Give an example of using a callback function to handle an asynchronous operation in JavaScript (Use any open api to make a call)
 
-const func=  (print)=>{
-    setTimeout(async ()=>{
-        const res = await fetch("https://currency-exchange.p.rapidapi.com/listquotes", {
-       headers: {
-        "X-RapidAPI-Key": "5f4516e06cmshfd4c3e5f19ad444p1ce5fbjsna35db917957a",
-        "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
+const func = async (print) => {
+  try {
+    const res = await fetch(
+      "https://currency-exchange.p.rapidapi.com/listquotes",
+      {
+        headers: {
+          "X-RapidAPI-Key":
+            "5f4516e06cmshfd4c3e5f19ad444p1ce5fbjsna35db917957a",
+          "X-RapidAPI-Host": "currency-exchange.p.rapidapi.com",
         },
-        });
-        const data =  res.json();
-        print(data);
+      }
+    );
+    const data = await res.json(); 
+    print(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+};
 
-    },1000)
-    
-}
-
-func((data)=>{
-    console.log(data);
-})
+func((data) => {
+  console.log(data);
+});
 
 /*output=[
   'SGD', 'MYR', 'EUR',
